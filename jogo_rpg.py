fda331091ac0ed452788ca92a222c32df9167da5
BrunoSS <simoes.bruno10@gmail.com>
2023-03-23 22:44:35 -0300

    Este código é um jogo de RPG simples em que o jogador enfrenta um monstro.
    Este código é um jogo de RPG simples em que o jogador enfrenta um monstro. A classe <strong>‘SerVivo’</strong> define as características de um ser vivo, como pontos de vida e pontos de ataque. A classe <strong> ‘Monstro’</strong> é uma subclasse de <strong> ‘SerVivo’</strong> e representa um monstro no jogo, com um nome específico. Há também duas subclasses de <strong> ‘Monstro’</strong>, <strong> ‘Lobo’</strong> e <strong> ‘Goblin’</strong>, cada uma com suas próprias características.<br><br>
    A função <strong> ‘batalhar’</strong> é responsável por gerenciar a batalha entre o jogador e o monstro. Ela imprime os pontos de vida do jogador e do monstro no início, e em seguida, os dois começam a se atacar alternadamente até que a vida de um dos dois chegue a zero. Quando a batalha termina, é exibida uma mensagem informando se o jogador ganhou ou perdeu.<br><br>
    O código começa perguntando ao jogador sobre as características do monstro que ele deseja enfrentar. Com base nas respostas, o código cria uma instância da classe <strong> ‘Lobo’</strong> ou <strong> ‘Goblin’</strong> com pontos de vida e pontos de ataque aleatórios. Em seguida, cria uma instância da classe <strong> ‘SerVivo’</strong> para representar o jogador, com pontos de vida e pontos de ataque aleatórios. Por fim, chama a função <strong> ‘batalhar’</strong> para começar a batalha.

--- jogo_rpg.py
+++ jogo_rpg.py
@@ -1,46 +1,75 @@
 import random
 
 class SerVivo:
-    def __init__(self):
-        self.vida = 100
-        self.ataque = random.randint(5, 15)
-        self.defesa = random.randint(5, 15)
+    def __init__(self, pontos_vida, pontos_ataque):
+        self.pontos_vida = pontos_vida
+        self.pontos_ataque = pontos_ataque
+    
+    def atacar(self):
+        return random.randint(1, self.pontos_ataque)
+    
+    def defender(self, dano):
+        self.pontos_vida -= dano
 
-    def atacar(self, outro):
-        dano = self.ataque - outro.defesa
-        if dano > 0:
-            outro.vida -= dano
-            print(f"{self.__class__.__name__} causou {dano} pontos de dano em {outro.__class__.__name__}")
-        else:
-            print(f"{self.__class__.__name__} não causou dano em {outro.__class__.__name__}")
-
-class Personagem(SerVivo):
-    def __init__(self, nome):
-        super().__init__()
+class Monstro(SerVivo):
+    def __init__(self, nome, pontos_vida, pontos_ataque):
+        super().__init__(pontos_vida, pontos_ataque)
         self.nome = nome
 
-class Lobo(SerVivo):
-    def __init__(self, forca):
-        super().__init__()
-        self.forca = forca
+class Lobo(Monstro):
+    def __init__(self, tamanho, pontos_vida, pontos_ataque):
+        nome = f"Lobo {tamanho}"
+        super().__init__(nome, pontos_vida, pontos_ataque)
+        self.forca = random.randint(1, pontos_ataque)
 
-    def atacar(self, outro):
-        dano = self.ataque * self.forca - outro.defesa
-        if dano > 0:
-            outro.vida -= dano
-            print(f"{self.__class__.__name__} causou {dano} pontos de dano em {outro.__class__.__name__}")
-        else:
-            print(f"{self.__class__.__name__} não causou dano em {outro.__class__.__name__}")
+class Goblin(Monstro):
+    def __init__(self, tamanho, pontos_vida, pontos_ataque):
+        nome = f"Goblin {tamanho}"
+        super().__init__(nome, pontos_vida, pontos_ataque)
+        self.inteligencia = random.randint(1, pontos_ataque)
 
-class Goblin(SerVivo):
-    def __init__(self, inteligencia):
-        super().__init__()
-        self.inteligencia = inteligencia
+def batalhar(jogador, monstro):
+    print(f"Jogador: {jogador.pontos_vida} HP | Monstro: {monstro.pontos_vida} HP")
+    while jogador.pontos_vida > 0 and monstro.pontos_vida > 0:
+        ataque_jogador = jogador.atacar()
+        print(f"Jogador atacou o {monstro.nome} com {ataque_jogador} de dano!")
+        monstro.defender(ataque_jogador)
+        if monstro.pontos_vida <= 0:
+            break
+        ataque_monstro = monstro.atacar()
+        print(f"{monstro.nome} atacou o jogador com {ataque_monstro} de dano!")
+        jogador.defender(ataque_monstro)
+    if jogador.pontos_vida <= 0:
+        print("Você perdeu!")
+    else:
+        print(f"{monstro.nome} foi derrotado!")
 
-    def atacar(self, outro):
-        dano = self.ataque + self.inteligencia - outro.defesa
-        if dano > 0:
-            outro.vida -= dano
-            print(f"{self.__class__.__name__} causou {dano} pontos de dano em {outro.__class__.__name__}")
-        else:
-            print(f"{self.__class__.__name__} não causou dano em {outro.__class__.__name__}")
+# Exemplo de uso
+print("Bem-vindo ao jogo RPG!")
+tamanho_monstro = input("Escolha o tamanho do monstro (grande, médio ou pequeno): ")
+if tamanho_monstro == "grande":
+    pontos_vida_monstro = random.randint(20, 30)
+    pontos_ataque_monstro = random.randint(5, 10)
+elif tamanho_monstro == "médio":
+    pontos_vida_monstro = random.randint(10, 20)
+    pontos_ataque_monstro = random.randint(3, 7)
+else:
+    pontos_vida_monstro = random.randint(5, 10)
+    pontos_ataque_monstro = random.randint(1, 5)
+tipo_monstro = input("Escolha o tipo de monstro (lobo ou goblin): ")
+if tipo_monstro == "lobo":
+    monstro = Lobo(tamanho_monstro, pontos_vida_monstro, pontos_ataque_monstro)
+elif tipo_monstro == "goblin":
+    monstro = Goblin(tamanho_monstro, pontos_vida_monstro, pontos_ataque_monstro)
+else:
+    print("Tipo de monstro inválido!")
+    exit()
+    
+print(f"Você está enfrentando um {monstro.nome} com {monstro.pontos_vida} HP e {monstro.pontos_ataque} pontos de ataque!")
+pontos_vida_jogador = random.randint(30, 50)
+pontos_ataque_jogador = random.randint(8, 12)
+jogador = SerVivo(pontos_vida_jogador, pontos_ataque_jogador)
+
+print(f"Você tem {jogador.pontos_vida} HP e {jogador.pontos_ataque} pontos de ataque!")
+input("Pressione Enter para começar a batalha...")
+batalhar(jogador, monstro)
